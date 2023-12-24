4a2a1bbcbcee4e90e1545c859bc35ed9c7ead6bd
oaragonga <oaragonga@sefin.oaxaca>
2023-12-24 12:29:21 -0600

    configuracion del inicio de sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inovations-app';
+  @ViewChild('sidenav')
+  sidenav!: MatSidenav;
+
+  isSidenavOpen = true;
+  toggleSidenav() {
+    console.log(this.sidenav);
+    this.sidenav.toggle();
+    this.isSidenavOpen = this.sidenav.opened;
+  }
 }
